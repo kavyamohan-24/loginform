79e306f228007c138968abaf8c01dfdce8390857
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-05-06 00:12:47 +0530

    login form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './components/LoginForm';
+import SearchForm from './components/SearchForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+      <SearchForm/>
+    </div>
+  );
+}
+
+export default App;
